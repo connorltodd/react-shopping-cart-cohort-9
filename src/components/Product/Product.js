a318ae2c0aefc900ba051e10babf71cb7d3416b1
Connor Todd <connortodd0304@gmail.com>
2022-08-10 18:39:51 +0100

    week 14 completed

---
+++ src/components/Product/Product.js
@@ -0,0 +1,15 @@
+import './Product.css';
+
+function Product (props) {
+    console.log(props)
+    return (
+        <div>
+            <img className='product-image' src={props.image} alt={props.title}  />
+            <h1>This is our newest {props.title}</h1>
+            <p>Category: {props.category}</p>
+            <p>Price: {props.price}</p>
+        </div>
+    )
+}
+
+export default Product;
